934e68b2bf12782d58ab99e83e840e93590c2713
Krxyzn <72651636+Krxyzn@users.noreply.github.com>
2024-04-15 20:17:49 -0700

    RMX3261 : Device.mk : Added missing part
    Signed-off-by: Krxyzn <72651636+Krxyzn@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -1,3 +1,63 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
+# A/B
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.2-impl \
+    android.hardware.boot@1.2-impl.recovery \
+    android.hardware.boot@1.2-service
+
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=ext4 \
+    POSTINSTALL_OPTIONAL_vendor=true
+
+PRODUCT_PACKAGES += \
+    checkpoint_gc \
+    otapreopt_script
+
+# API levels
+PRODUCT_SHIPPING_API_LEVEL := 30
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.1-impl-mock \
+    fastbootd
+
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
+
+# Overlays
+PRODUCT_ENFORCE_RRO_TARGETS := *
+
+# Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Product characteristics
+PRODUCT_CHARACTERISTICS := default
+
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
@@ -92,8 +152,16 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt \
 
-# Ramdisk
+# Rootdir
 PRODUCT_PACKAGES += \
+    log_to_csv.sh \
+    loading.sh \
+    para.sh \
+    total.sh \
+    engineer_vendor_shell.sh \
+
+PRODUCT_PACKAGES += \
+    fstab.ums512_2h10 \
     init.S19610AA1.rc \
     init.S19610AA1.usb.rc \
     init.cali.rc \
@@ -108,15 +176,20 @@ PRODUCT_PACKAGES += \
     init.ums512_20c10.usb.rc \
     init.ums512_2h10.rc \
     init.ums512_2h10.usb.rc \
-    fstab.S19610AA1 \
-    fstab.cali \
-    fstab.enableswap \
-    fstab.factorytest \
-    fstab.ums512_1h10 \
-    fstab.ums512_1h10_go \
-    fstab.ums512_20c10 \
-    fstab.ums512_2h10 \
-    ueventd.qcom.rc \
+    init.recovery.S19610AA1.rc \
+    init.recovery.common.rc \
+    init.recovery.ums512_1h10.rc \
+    init.recovery.ums512_1h10_go.rc \
+    init.recovery.ums512_20c10.rc \
+    init.recovery.ums512_2h10.rc \
+    ueventd.S19610AA1.rc \
+    ueventd.ums512_1h10.rc \
+    ueventd.ums512_1h10_go.rc \
+    ueventd.ums512_20c10.rc \
+    ueventd.ums512_2h10.rc \
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/fstab.ums512_2h10:$(TARGET_VENDOR_RAMDISK_OUT)/first_stage_ramdisk/fstab.ums512_2h10
 
 # Seccomp
 PRODUCT_COPY_FILES += \
